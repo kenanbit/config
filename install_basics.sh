3807dfdf626c493ec04cd4675182163af849211b
Kenan Bitikofer <kbitikofer@gmail.com>
2020-03-06 18:11:08 -0500

    Install vimwiki, tree

--- install_basics.sh
+++ install_basics.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 USER=`whoami`
 echo 'Installing basic software stuffs'
-sudo apt install vim-gtk tmux zsh
+sudo apt install vim-gtk tmux zsh tree
 
 echo 'Installing oh-my-zsh'
 yes n | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo 'Changing default shell to zsh'
 sudo chsh -s /bin/zsh $USER
+
+echo 'Installing vimwiki'
+git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki
